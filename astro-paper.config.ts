16bf9380bcd51087a70f73cecc5c7aecaa07c980
Muelsyseqwq <1615844471@qq.com>
2026-08-17 11:02:03 +0800

    Build Muelsyse Astro personal site

---
+++ astro-paper.config.ts
@@ -0,0 +1,37 @@
+import { defineAstroPaperConfig } from "./src/types/config";
+
+export default defineAstroPaperConfig({
+  site: {
+    url: "https://muelsyseqwq.github.io/",
+    title: "Muelsyse",
+    description: "A student from Beijing Forestry University.",
+    author: "Muelsyse",
+    profile: "https://github.com/Muelsyseqwq",
+    ogImage: "default-og.jpg",
+    lang: "zh-CN",
+    timezone: "Asia/Shanghai",
+    dir: "ltr",
+  },
+  posts: {
+    perPage: 4,
+    perIndex: 4,
+    scheduledPostMargin: 15 * 60 * 1000,
+  },
+  features: {
+    lightAndDarkMode: true,
+    dynamicOgImage: false,
+    showArchives: true,
+    showBackButton: true,
+    editPost: { enabled: false },
+    search: "pagefind",
+  },
+  socials: [{ name: "github", url: "https://github.com/Muelsyseqwq" }],
+  shareLinks: [
+    { name: "whatsapp", url: "https://wa.me/?text=" },
+    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
+    { name: "x",        url: "https://x.com/intent/post?url=" },
+    { name: "telegram", url: "https://t.me/share/url?url=" },
+    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
+    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+  ],
+});
